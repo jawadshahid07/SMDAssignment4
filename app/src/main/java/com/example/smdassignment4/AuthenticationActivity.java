80e54d71e72fe1077cda986c3fd1c24a5ed6be61
jawadshahid07 <jawad.shahid70@outlook.com>
2024-11-27 18:46:13 +0500

    connected firebase

--- app/src/main/java/com/example/smdassignment4/AuthenticationActivity.java
+++ app/src/main/java/com/example/smdassignment4/AuthenticationActivity.java
@@ -1,24 +1,23 @@
 package com.example.smdassignment4;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.FirebaseApp;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_authentication);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        FirebaseApp.initializeApp(this);
+        if (FirebaseApp.getApps(this).size() > 0) {
+            Log.d("Firebase", "Firebase is connected successfully!");
+        }
     }
 }
