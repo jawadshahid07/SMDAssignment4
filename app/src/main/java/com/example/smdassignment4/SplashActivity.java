9fa6764578fea3c8a049ad3562677e87470f091e
jawadshahid07 <jawad.shahid70@outlook.com>
2024-11-27 18:28:09 +0500

    completed splash screen

--- app/src/main/java/com/example/smdassignment4/SplashActivity.java
+++ app/src/main/java/com/example/smdassignment4/SplashActivity.java
@@ -1,24 +1,36 @@
 package com.example.smdassignment4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ImageView logo = findViewById(R.id.logo);
+
+        // Load combined animation
+        Animation splashAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_animation);
+
+        // Start animation
+        logo.startAnimation(splashAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, AuthenticationActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000); // 5000ms = 5 seconds
     }
 }
